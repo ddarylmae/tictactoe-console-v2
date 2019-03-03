d34e0ee4f6219dbb5b16399cf8ae831050c8f42f
Daryl Dalan <diieem@outlook.com>
2019-03-04 10:36:12 +1300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TictactoeVer2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ TictactoeVer2Tests/TictactoeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace TictactoeVer2Tests
+{
+    public class TictactoeTests
+    {
+        [Fact]
+        public void ShouldStartWithPlayerX()
+        {
+            
+        }
+    }
+}
