d0fe929e8569221b9b40af718a345fb4874d7c70
Daryl Dalan <diieem@outlook.com>
2019-03-07 09:54:56 +1300

    Extracted IsColumnANumber

--- UserInputValidator.cs
+++ UserInputValidator.cs
@@ -14,12 +14,17 @@ namespace TictactoeVer2
             var coordinates = input.Split(',');
             return IsRowColumnPair(coordinates) && 
                    IsRowANumber(coordinates, out var row) &&
-                   int.TryParse(coordinates[1], out var column) && 
+                   IsColumnANumber(coordinates, out var column) && 
                    row > 0 && row < 4 && 
                    column > 0 && column < 4;
         }
 
-        private static bool IsRowANumber(string[] coordinates, out int row)
+        private bool IsColumnANumber(string[] coordinates, out int column)
+        {
+            return int.TryParse(coordinates[1], out column);
+        }
+
+        private bool IsRowANumber(string[] coordinates, out int row)
         {
             return int.TryParse(coordinates[0], out row);
         }
