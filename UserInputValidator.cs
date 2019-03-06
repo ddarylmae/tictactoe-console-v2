cb1310ee4dd8dfcb3afa651f945bc7c43c3f2616
Daryl Dalan <diieem@outlook.com>
2019-03-07 09:42:40 +1300

    Extracted IsValidCoordinate

--- UserInputValidator.cs
+++ UserInputValidator.cs
@@ -6,18 +6,22 @@ namespace TictactoeVer2
     {
         public bool IsValidInput(string input)
         {
-            var coordinates = input.Split(',');
-            
-            if (IsPlayerQuitting(input) || coordinates.Length == 2 && 
-                int.TryParse(coordinates[0], out var row) &&
-                int.TryParse(coordinates[1], out var column) && 
-                row > 0 && row < 4 && 
-                column > 0 && column < 4)
+            if (IsPlayerQuitting(input) || IsValidCoordinate(input))
             {
                 return true;
             }
             
             return false;
+        }
+
+        private static bool IsValidCoordinate(string input)
+        {
+            var coordinates = input.Split(',');
+            return coordinates.Length == 2 && 
+                   int.TryParse(coordinates[0], out var row) &&
+                   int.TryParse(coordinates[1], out var column) && 
+                   row > 0 && row < 4 && 
+                   column > 0 && column < 4;
         }
 
         private static bool IsPlayerQuitting(string input)
