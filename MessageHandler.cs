62b943549243f90af7ba7064a111d0fc9259953a
Daryl Dalan <diieem@outlook.com>
2019-03-12 10:16:24 +1300

    Added function for outputting message

--- MessageHandler.cs
+++ MessageHandler.cs
@@ -4,45 +4,47 @@ namespace TictactoeVer2
 {
     public class MessageHandler
     {
+        private void OutputMessage(string message) => Console.WriteLine(message);
+        
         public void WelcomeToGame()
         {
-            Console.WriteLine("Welcome to Tic Tac Toe!");
-            Console.WriteLine("Here's the current board: ");
+            OutputMessage("Welcome to Tic Tac Toe!");
+            OutputMessage("Here's the current board: ");
         }
 
         public void DisplayTakeTurn(Player currentPlayer)
         {
-            Console.WriteLine($"Player {currentPlayer} please enter a coord x,y to place your move or 'q' to give up: ");
+            OutputMessage($"Player {currentPlayer} please enter a coord x,y to place your move or 'q' to give up: ");
         }
 
         public void DisplayNoWinner()
         {
-            Console.WriteLine("Game has ended. No winner.");
+            OutputMessage("Game has ended. No winner.");
         }
 
         public void DisplayWinner(Player winner)
         {
-            Console.WriteLine($"Game has ended. Player {winner.ToString()} has won!");
+            OutputMessage($"Game has ended. Player {winner.ToString()} has won!");
         }
 
         public void DisplayMoveInvalid()
         {
-            Console.WriteLine("Move invalid. Try again.");
+            OutputMessage("Move invalid. Try again.");
         }
 
         public void DisplayMoveAccepted()
         {
-            Console.WriteLine("Move accepted.");
+            OutputMessage("Move accepted.");
         }
 
         public void DisplayCoordinateIsFilled()
         {
-            Console.WriteLine("Oh no, a piece is already at this place! Try again.");
+            OutputMessage("Oh no, a piece is already at this place! Try again.");
         }
 
         public void DisplayUserHasQuit(Player currentPlayer)
         {
-            Console.WriteLine($"Player {currentPlayer.ToString()} has quit.");
+            OutputMessage($"Player {currentPlayer.ToString()} has quit.");
         }
     }
 }
